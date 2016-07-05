395a0e333d8ba2701118979fa17437011490ac8c
1000ch <shogo.sensui@gmail.com>
2016-07-05 18:36:27 +0900

    Convert to function

--- index.js
+++ index.js
@@ -6,23 +6,10 @@ const mapStream = require('map-stream');
 const gutil = require('gulp-util');
 const filesize = require('filesize');
 const chalk = require('chalk');
-
-const Optimizer = require('./optimizer');
+const optimize = require('./optimize');
 const round10 = require('./round10');
 
 module.exports = function(options) {
-  const defaultOptions = {
-    pngquant       : true,
-    optipng        : false,
-    zopflipng      : true,
-    advpng         : true,
-    jpegRecompress : false,
-    jpegoptim      : true,
-    mozjpeg        : true,
-    gifsicle       : true,
-    svgo           : true
-  };
-
   return mapStream((file, callback) => {
     if (file.isNull()) {
       return callback(null, file);
@@ -39,39 +26,39 @@ module.exports = function(options) {
       return callback(null, file);
     }
 
-    let optimizer = new Optimizer({
-      src     : file.path,
-      buffer  : file.contents,
-      options : Object.assign({}, defaultOptions, options || {})
+    optimize(file.path, file.contents, Object.assign({
+      pngquant       : true,
+      optipng        : false,
+      zopflipng      : true,
+      advpng         : true,
+      jpegRecompress : false,
+      jpegoptim      : true,
+      mozjpeg        : true,
+      gifsicle       : true,
+      svgo           : true
+    }, options || {})).then(buffer => {
+      let original = fs.statSync(file.path).size;
+      let diff = original - buffer.length;
+      let diffPercent = round10(100 * (diff / original), -1);
+
+      if (diff <= 0) {
+        gutil.log(
+          chalk.green('- ') + file.relative + chalk.gray(' ->') +
+          chalk.gray(' Cannot improve upon ') + chalk.cyan(filesize(original))
+        );
+      } else {
+        file.contents = buffer;
+        gutil.log(
+          chalk.green('✔ ') + file.relative + chalk.gray(' ->') +
+          chalk.gray(' before=') + chalk.yellow(filesize(original)) +
+          chalk.gray(' after=') + chalk.cyan(filesize(buffer.length)) +
+          chalk.gray(' reduced=') + chalk.green.underline(filesize(diff) + '(' + diffPercent + '%)')
+        );
+      }
+
+      callback(null, file);
+    }).catch(error => {
+      callback(new gutil.PluginError('gulp-image', error));
     });
-
-    optimizer
-      .optimize()
-      .then(buffer => {
-        let original = fs.statSync(file.path).size;
-        let optimized = buffer.length;
-        let diff = original - optimized;
-        let diffPercent = round10(100 * (diff / original), -1);
-
-        if (diff <= 0) {
-          gutil.log(
-            chalk.green('- ') + file.relative + chalk.gray(' ->') +
-            chalk.gray(' Cannot improve upon ') + chalk.cyan(filesize(original))
-          );
-        } else {
-          file.contents = buffer;
-          gutil.log(
-            chalk.green('✔ ') + file.relative + chalk.gray(' ->') +
-            chalk.gray(' before=') + chalk.yellow(filesize(original)) +
-            chalk.gray(' after=') + chalk.cyan(filesize(optimized)) +
-            chalk.gray(' reduced=') + chalk.green.underline(filesize(diff) + '(' + diffPercent + '%)')
-          );
-        }
-
-        callback(null, file);
-      })
-      .catch(error => {
-        callback(new gutil.PluginError('gulp-image', error));
-      });
   }, 10);
 };
---
+++ optimize.js
@@ -0,0 +1,198 @@
+'use strict';
+
+const path = require('path');
+const execa = require('execa');
+const execBuffer = require('exec-buffer');
+
+function optipng(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('optipng-bin'),
+    args  : [
+      '-i 1',
+      '-strip all',
+      '-fix',
+      '-o7',
+      '-force',
+      '-out', execBuffer.output,
+      execBuffer.input
+    ]
+  });
+}
+
+function pngquant(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('pngquant-bin'),
+    args  : [
+      '--speed=1',
+      '--force',
+      '256',
+      '--output',
+      execBuffer.output,
+      execBuffer.input
+    ]
+  });
+}
+
+function advpng(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('advpng-bin'),
+    args  : [
+      '--recompress',
+      '--shrink-extra',
+      execBuffer.input,
+      execBuffer.output
+    ]
+  });
+}
+
+function pngcrush(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('pngcrush-bin'),
+    args  : [
+      '-rem alla',
+      '-rem text',
+      '-brute',
+      '-reduce',
+      execBuffer.input,
+      execBuffer.output
+    ]
+  });
+}
+
+function zopflipng(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('zopflipng-bin'),
+    args  : [
+      '-y',
+      '-m',
+      '--lossy_8bit',
+      '--lossy_transparent',
+      execBuffer.input,
+      execBuffer.output
+    ]
+  });
+}
+
+function gifsicle(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('gifsicle'),
+    args  : [
+      '--optimize',
+      '--output',
+      execBuffer.output,
+      execBuffer.input
+    ]
+  });
+}
+
+function jpegtran(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('jpegtran-bin'),
+    args  : [
+      '-optimize',
+      '-progressive',
+      '-outfile',
+      execBuffer.output,
+      execBuffer.input
+    ]
+  });
+}
+
+function jpegRecompress(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('jpeg-recompress-bin'),
+    args  : [
+      '--strip',
+      '--quality', 'medium',
+      '--min', 40,
+      '--max', 80,
+      execBuffer.input,
+      execBuffer.output
+    ]
+  });
+}
+
+function jpegoptim(buffer) {
+  return execa(require('jpegoptim-bin'), [
+    '--strip-all',
+    '--strip-iptc',
+    '--strip-icc',
+    '--stdin',
+	  '--stdout'
+  ], {
+    input : buffer
+  });
+}
+
+function mozjpeg(buffer) {
+  return execBuffer({
+    input : buffer,
+    bin   : require('mozjpeg'),
+    args  : [
+      '-optimize',
+      '-progressive',
+      '-outfile',
+      execBuffer.output,
+      execBuffer.input
+    ]
+  });
+}
+
+function svgo(buffer, options) {
+  let args = [
+    '--input', execBuffer.input,
+    '--output', execBuffer.output
+  ];
+
+  if (options.enable) {
+    args.push(`--enable=${options.enable}`);
+  }
+
+  if (options.disable) {
+    args.push(`--disable=${options.disable}`);
+  }
+
+  return execBuffer({
+    input : buffer,
+    bin   : `${__dirname}/node_modules/svgo/bin/svgo`,
+    args  : args
+  });
+}
+
+module.exports = function(filePath, buffer, options) {
+  let promises = [];
+  let extension = path.extname(filePath).toLowerCase();
+
+  if (extension === '.jpeg' || extension === '.jpg') {
+    return Promise.resolve(buffer)
+      .then(buffer => options.jpegRecompress ? jpegRecompress(buffer) : buffer)
+      .then(buffer => options.jpegoptim ? jpegoptim(buffer) : buffer)
+      .then(buffer => options.mozjpeg ? mozjpeg(buffer) : buffer)
+      .catch(error => console.error(error));
+  } else if (extension === '.png') {
+    return Promise.resolve(buffer)
+      .then(buffer => options.pngquant ? pngquant(buffer) : buffer)
+      .then(buffer => options.optipng ? optipng(buffer) : buffer)
+      .then(buffer => options.zopflipng ? zopflipng(buffer) : buffer)
+      .then(buffer => options.advpng ? advpng(buffer) : buffer)
+      .catch(error => console.error(error));
+  } else if (extension === '.gif') {
+    return Promise.resolve(buffer)
+      .then(buffer => options.gifsicle ? gifsicle(buffer) : buffer)
+      .catch(error => console.error(error));
+  } else if (extension === '.svg') {
+    return Promise.resolve(buffer)
+      .then(buffer => options.svgo ? svgo(buffer, options.svgo) : buffer)
+      .catch(error => console.error(error));
+  }
+
+  return Promise.resolve(buffer);
+};
