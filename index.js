b06ed928f6592252a9c73c1092728406d82d55ac
1000ch <shogo.sensui@gmail.com>
2018-02-09 14:51:25 +0900

    Cosme

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const { extname } = require('path');
 const through2 = require('through2-concurrent');
 const PluginError = require('plugin-error');
 const colors = require('ansi-colors');
-const log = require('fancy-log');
+const fancyLog = require('fancy-log');
 const replaceExtension = require('replace-ext');
 const filesize = require('filesize');
 const { round10 } = require('round10');
 const optimize = require('./optimize');
 
-function NOOP(){};
-
 const SUPPORTED_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.svg'];
+const NOOP = () => {};
 
 module.exports = options => through2.obj({
   maxConcurrency: options ? options.concurrent : null
@@ -20,16 +19,16 @@ module.exports = options => through2.obj({
   if (file.isNull()) {
     return callback(null, file);
   }
-  const _log = options && options.quiet ? NOOP : log;
 
   if (file.isStream()) {
     return callback(new Error('gulp-image: Streaming is not supported'));
   }
 
+  const log = options && options.quiet ? NOOP : fancyLog;
   const extension = extname(file.path).toLowerCase();
 
   if (!SUPPORTED_EXTENSIONS.includes(extension)) {
-    _log('gulp-image: Skipping unsupported image ' + colors.blue(file.relative));
+    log('gulp-image: Skipping unsupported image ' + colors.blue(file.relative));
     return callback(null, file);
   }
 
@@ -49,14 +48,14 @@ module.exports = options => through2.obj({
     const diffPercent = round10(100 * (diff / before), -1);
 
     if (diff <= 0) {
-      _log(
+      log(
         colors.green('- ') + file.relative + colors.gray(' ->') +
         colors.gray(' Cannot improve upon ') + colors.cyan(filesize(before))
       );
     } else {
       file.contents = buffer;
 
-      _log(
+      log(
         colors.green('✔ ') + file.relative + colors.gray(' ->') +
         colors.gray(' before=') + colors.yellow(filesize(before)) +
         colors.gray(' after=') + colors.cyan(filesize(after)) +
